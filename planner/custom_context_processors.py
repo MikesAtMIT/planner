102669687e9e6e67a449660b2917e0feb487d65b
MikesAtMIT <michael.lee1@gmail.com>
2017-01-31 21:47:29 -0800

    fix tab

--- planner/custom_context_processors.py
+++ planner/custom_context_processors.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 
 def navbar_data(request):
-  projects = Project.objects.all()
-  users = User.objects.all()
-  context = {
-    'all_projects': projects,
-    'all_users': users,
-  }
-  return context
+    projects = Project.objects.all()
+    users = User.objects.all()
+    context = {
+        'all_projects': projects,
+        'all_users': users,
+    }
+    return context
